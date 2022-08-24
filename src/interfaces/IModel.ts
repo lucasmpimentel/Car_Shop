71bc95b2ac29b8b888a57f8d95a195c3dab859e8
Lucas Pimentel <lucas.mirandapimentel@gmail.com>
2022-08-24 06:00:51 +0000

    fix: IModel Export

--- src/interfaces/IModel.ts
+++ src/interfaces/IModel.ts
@@ -1,4 +1,4 @@
-export default interface IModel<T> {
+export interface IModel<T> {
   create(obj: T): Promise<T>,
   readOne(_id: string): Promise<T | null>,
   update(_id: string, obj: T): Promise<T | null>,
